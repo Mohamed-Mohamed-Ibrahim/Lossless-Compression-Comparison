69738284c444fbf12885d798289e33eff9f9ff58
Mohamed Mohamed Ibrahim <mohamedgawad600@gmail.com>
2026-05-28 15:03:11 +0300

    Combing deflate and lz77 in compressor.

--- deflate/compressor.py
+++ deflate/compressor.py
@@ -1,9 +1,18 @@
-from template.compressor import Compressor
-
+from template import Compressor
+from deflate.deflate import (
+    get_lz77_tokens,
+    tokens_to_events,
+    count_frequencies,
+    deflate_decompress,
+)
 
 class DeflateCompressor(Compressor):
-    def compress(self, data):
-        pass
+    def compress(self, data: bytes) -> bytes:
+        tokens = get_lz77_tokens(data)
 
-    def decompress(self, compressedData):
+        events = tokens_to_events(tokens)
+
+        lit_freq, dist_freq = count_frequencies(events)
+
+    def decompress(self, data: bytes) -> bytes:
         pass
